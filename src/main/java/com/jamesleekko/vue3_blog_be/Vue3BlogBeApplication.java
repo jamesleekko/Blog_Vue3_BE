18f5cd5cbae09c4eac23ac356679a242eda4bd6f
jamesleekko <jamesleecode@gmail.com>
2022-07-18 18:40:18 +0800

    spring boot init

---
+++ src/main/java/com/jamesleekko/vue3_blog_be/Vue3BlogBeApplication.java
@@ -0,0 +1,13 @@
+package com.jamesleekko.vue3_blog_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vue3BlogBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vue3BlogBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamesleekko/vue3_blog_be/Vue3BlogBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamesleekko.vue3_blog_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vue3BlogBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
